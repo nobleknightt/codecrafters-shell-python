1180a9fe00ebf040780d0684a384153f259525fb
nobleknightt <60701544+nobleknightt@users.noreply.github.com>
2024-06-23 19:53:55 +0530

    handle missing commands (made it long running)

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        sys.stdout.write(f"{command}: command not found")
+        sys.stdout.flush()
 
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
-    sys.stdout.flush()
-    
 
 if __name__ == "__main__":
     main()
