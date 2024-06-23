e3549f57d1340d96e9dcc69dde28729d745fef2a
nobleknightt <60701544+nobleknightt@users.noreply.github.com>
2024-06-23 20:53:53 +0530

    the type builtin: executable files (fixed error - directory not found)

--- app/main.py
+++ app/main.py
@@ -7,40 +7,45 @@ from pathlib import Path
 shell_builtins = {"exit", "echo", "type"}
 
 def main():
-    while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        
-        command = input().split()
-        
-        if command[0] == "exit":
-            sys.exit(0)
+    try:
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+            
+            command = input().split()
+            
+            if command[0] == "exit":
+                sys.exit(0)
 
-        if command[0] == "echo":
-            sys.stdout.write(" ".join(command[1:]))    
-        elif command[0] == "type":
-            if command[1] in shell_builtins:
-                sys.stdout.write(f"{command[1]} is a shell builtin")
-            else:
-                PATH = os.environ.get('PATH')
-                found = False
-                if PATH is not None:
-                    paths = PATH.split(":")
-                    for path in paths:                        
-                        for file in Path(path).iterdir():
-                            if str(file).endswith(f"/{command[1]}"):
-                                found = True
-                                sys.stdout.write(f"{command[1]} is {path}/{command[1]}")
+            if command[0] == "echo":
+                sys.stdout.write(" ".join(command[1:]))    
+            elif command[0] == "type":
+                if command[1] in shell_builtins:
+                    sys.stdout.write(f"{command[1]} is a shell builtin")
+                else:
+                    PATH = os.environ.get('PATH')
+                    found = False
+                    if PATH is not None:
+                        paths = PATH.split(":")
+                        for path in paths:
+                            dir = Path(path)
+                            if dir.exists() and dir.is_dir():                       
+                                for file in Path(path).iterdir():
+                                    if str(file).endswith(f"/{command[1]}"):
+                                        found = True
+                                        sys.stdout.write(f"{command[1]} is {path}/{command[1]}")
+                                        break
+                            if found:
                                 break
-                        if found:
-                            break
-                if not found:
-                    sys.stdout.write(f"{command[1]}: not found")
-        else:
-            sys.stdout.write(f"{command[0]}: command not found")
-    
-        sys.stdout.write("\n")
-        sys.stdout.flush()
+                    if not found:
+                        sys.stdout.write(f"{command[1]}: not found")
+            else:
+                sys.stdout.write(f"{command[0]}: command not found")
+        
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
